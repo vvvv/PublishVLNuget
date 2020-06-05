6730cf73ec03085b4d61cbb76fb76558aba80d18
sebescudie <seb.espm@gmail.com>
2020-06-05 15:16:48 +0200

    rework

--- index.js
+++ index.js
@@ -1,72 +1,99 @@
 const core = require('@actions/core');
 const download = require('download-file')
 const semver = require('semver')
-const run = require('child_process').spawnSync
+const spawn = require('child_process').spawnSync
 
-try
-{
-    const nuspec = core.getInput('NUSPEC', { required: true })
-    const solution = core.getInput('SOLUTION')
-    const icon_src = core.getInput('ICON_SRC')
-    const icon_dst = core.getInput('ICON_DST')
-    const version = core.getInput('VERSION')
-    const auto_increment_patch = core.getInput('AUTO_PATCH_NUMBER')
-    const nuget_key = core.getInput('NUGET_KEY')
-    const nuget_feed = core.getInput('NUGET_FEED')
-    const use_symbols = core.getInput('USE_SYMBOLS')
-
-    // msbuild
-    if(solution){
-        run(`msbuild ${solution} /t:Build /v:m /m /restore /p:Configuration=Release`)
+class Action{
+    constructor(){
+        this.nuspec = core.getInput('NUSPEC', { required: true })
+        this.solution = core.getInput('SOLUTION')
+        this.icon_src = core.getInput('ICON_SRC')
+        this.icon_dst = core.getInput('ICON_DST')
+        this.version = core.getInput('VERSION')
+        this.auto_increment_patch = core.getInput('AUTO_PATCH_NUMBER')
+        this.nuget_key = core.getInput('NUGET_KEY')
+        this.nuget_feed = core.getInput('NUGET_FEED')
+        this.use_symbols = core.getInput('USE_SYMBOLS')
     }
 
-    // icon
-    if(icon_src){
-        if(!icon_dst){
-            // you asked to download an icon but did not tell me where to put it
+    run(cmd){
+        console.log(`Now running ${cmd}`)
+        this.exec(cmd, { encoding: "utf-8", stdio: [process.stdin, process.stdout, process.stderr] })
+    }
+
+    exec(cmd, options){
+        return spawn(cmd, options);
+    }
+
+    buildSolution(){
+        this.run(`msbuild ${this.solution} /t:Build /v:m /m /restore /p:Configuration=Release`)
+    }
+
+    downloadIcon(){
+        if(!this.icon_dst){
             core.error('You provided an icon source but no destination, won\'t be able to pack your nuget!')
             core.setFailed('Please provide a destination for your icon')
-        }else
-        {
-            // alrighty, proceed
+        }else{
             var options = {
-                directory: icon_dst
+                directory: this.icon_dst
             }
-            download(icon_src, options, function(err){
+            download(this.icon_src, options, function(err){
                 if (err) throw err
-                console.log("Something went wrong downloading the file")
+                core.setFailed('Cannot download the icon')
             })
         }
     }
 
-    // pack der nuget
-    if(version){
-        sem = semver.parse(version)
-        if(sem == null){
-            core.error('The version you provided cannot be parsed to semver, please advise')
-            core.setFailed('Please provide a valid semver version')
-        }else{
-            if(auto_increment_patch == true){
-                // user wants to use github run number, let's set our semver object
-                sem.patch = process.env.GITHUB_RUN_NUMBER
+    packNuget(){
+        if(this.version){
+            sem = semver.parse(version)
+            if(sem == null){
+                core.error('The version you provided cannot be parsed to semver, please advise')
+                core.setFailed('Please provide a valid semver version')
+            }else{
+                if(this.auto_increment_patch == true){
+                    // user wants to use github run number, let's set our semver object
+                    sem.patch = process.env.GITHUB_RUN_NUMBER
+                }
+                // pack with semver object
+                run(`nuget pack ${nuspec} -Version ${sem.version}`)
             }
-            // pack with semver object
-            run(`nuget pack ${nuspec} -Version ${sem.version}`)
+        }else{
+            // pack with nuspec version
+            run(`nuget pack ${nuspec}`)
         }
-    }else{
-        // pack with nuspec version
-        run(`nuget pack ${nuspec}`)
     }
 
-    // push der nuget
-    if(use_symbols){
-        run(`nuget push *.nupkg ${nuget_key} -src ${nuget_feed}`)
-    }else{
-        run(`nuget push *.nupkg ${nuget_key} -src ${nuget_feed} -NoSymbols`)
+    pushNuget(){
+        if(this.use_symbols){
+            run(`nuget push *.nupkg ${nuget_key} -src ${nuget_feed}`)
+        }else{
+            run(`nuget push *.nupkg ${nuget_key} -src ${nuget_feed} -NoSymbols`)
+        }
+    }
+
+    run(){
+        // Build VS solution
+        if(this.solution){
+            this.buildSolution()
+        }else{
+            core.info('Your nuget does not have a VS solution')
+        }
+
+        // Retrieve icon
+        if(this.icon_src){
+            this.downloadIcon()
+        }else{
+            core.info('Your did not specify any remote icon')
+        }
+
+        // Pack nuget
+        this.packNuget()
+
+        // Push nuget
+        this.pushNuget()
     }
     
 }
-catch
-{
-    // error handling
-}
+
+new Action().run()
